dd86f2ee862d74e5aa83373b62c37c59eb766348
Jason McMains <jmcmains@gmail.com>
2012-01-31 13:21:05 -0500

    added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ExerciseApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/help"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

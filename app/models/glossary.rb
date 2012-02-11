c7a4e0c5f6597976a9ef848777fc8ba2d3120f7a
Jason McMains <jmcmains@gmail.com>
2012-02-10 23:41:05 -0500

    modified glossary to remove index

--- app/models/glossary.rb
+++ app/models/glossary.rb
@@ -1,7 +1,4 @@
 class Glossary < ActiveRecord::Base
 	has_many :glossary_pictures, :foreign_key => "glossary_id",
                            		 :dependent => :destroy
- 	searchable do
-		text :term, :description
-	end
 end

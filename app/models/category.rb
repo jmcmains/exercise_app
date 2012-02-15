aa227cc23b946cd6e55027eef08529b9a1c4cd7b
Jason McMains <jmcmains@gmail.com>
2012-02-15 12:59:46 -0500

    Formatting fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,4 @@
 class Category < ActiveRecord::Base
-
-	
 	attr_accessible :name
 	has_many :category_exercises, :foreign_key => "category_id",
                                    			 :class_name => "ExerciseCategory",
